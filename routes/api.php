16fe7222f672aa0f0f5896cbc4151762d2dbdcf3
Manian <manianrezaie@gmail,com>
2019-08-26 22:28:15 +0430

    create test case naming for api routes

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/players', 'Admin\PlayerController@apiListAll');
-Route::get('/player/{id}', 'Admin\PlayerController@apiShow');
-Route::get('/teams', 'Admin\TeamController@apiListAll');
-Route::get('/team/{id}', 'Admin\TeamController@apiShow');
+Route::get('/players', 'Admin\PlayerController@apiListAll')->name("api_all_players");
+Route::get('/player/{id}', 'Admin\PlayerController@apiShow')->name("api_get_player");
+Route::get('/teams', 'Admin\TeamController@apiListAll')->name("api_all_teams");
+Route::get('/team/{id}', 'Admin\TeamController@apiShow')->name("api_get_team");
